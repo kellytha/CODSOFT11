af00909675a18c2c5f354ecf6f7228bae2caa44e
kellytha <154939019+kellytha@users.noreply.github.com>
2024-01-15 15:57:44 +0200

    Update quizgames.py

--- quizgames.py
+++ quizgames.py
@@ -15,6 +15,13 @@ if answer.lower() == "central processing unit":
     score += 1
 else:
     print("incorrect!")
+answer = input("what does tv stand for")
+if answer.lower() == " televison ":
+    print("correct")
+    score += 1
+else:
+    print("incorrect")
+    
 
 print("you got" + str(score) + "questions correct!")
 print("you got" + str((score/ 1) * 100) + "%.")
