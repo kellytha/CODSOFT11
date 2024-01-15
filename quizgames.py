2704ee92fdbc9f66798cbd0307b69a3877529059
kellytha <154939019+kellytha@users.noreply.github.com>
2024-01-15 15:51:21 +0200

    Add files via upload

---
+++ quizgames.py
@@ -0,0 +1,21 @@
+print("Welcome to my computer quiz!")
+
+playing = input(" Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("okay let's play ;)")
+score = 0
+
+
+answer = input("what does cpu stand for? ")
+if answer.lower() == "central processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+print("you got" + str(score) + "questions correct!")
+print("you got" + str((score/ 1) * 100) + "%.")
+#we can add our own questions by copying and pasting the first code
